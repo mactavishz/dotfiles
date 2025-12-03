6c18cff8c3ceb08109a0cdf5c929926daf572069
mactavishz <maczhanchao@yahoo.com>
2025-12-04 00:25:18 +0100

    Update linter logic

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -1,16 +1,58 @@
 return {
-
   { -- Linting
     'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
     cond = vim.g.vscode == nil,
-    config = function()
-      local lint = require 'lint'
-      lint.linters_by_ft = {
+    opts = {
+      events = { 'BufReadPost', 'BufWritePost', 'InsertLeave' },
+      linters_by_ft = {
         markdown = { 'markdownlint' },
         go = { 'golangcilint' },
         python = { 'ruff' },
-      }
+      },
+    },
+    config = function(_, opts)
+      local lint = require 'lint'
+
+      local M = {}
+
+      function M.debounce(ms, fn)
+        local timer = vim.uv.new_timer()
+        return function(...)
+          local argv = { ... }
+          timer:start(ms, 0, function()
+            timer:stop()
+            vim.schedule_wrap(fn)(unpack(argv))
+          end)
+        end
+      end
+
+      function M.lint()
+        -- Use nvim-lint's logic first:
+        -- * checks if linters exist for the full filetype first
+        -- * otherwise will split filetype by "." and add all those linters
+        -- * this differs from conform.nvim which only uses the first filetype that has a formatter
+        local names = lint._resolve_linter_by_ft(vim.bo.filetype)
+
+        -- Create a copy of the names table to avoid modifying the original.
+        names = vim.list_extend({}, names)
+
+        -- Add fallback linters.
+        if #names == 0 then
+          vim.list_extend(names, lint.linters_by_ft['_'] or {})
+        end
+
+        -- Add global linters.
+        vim.list_extend(names, lint.linters_by_ft['*'] or {})
+
+        -- Run linters.
+        if #names > 0 then
+          lint.try_lint(names)
+        end
+      end
+      vim.api.nvim_create_autocmd(opts.events, {
+        group = vim.api.nvim_create_augroup('nvim-lint', { clear = true }),
+        callback = M.debounce(100, M.lint),
+      })
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
@@ -43,21 +85,6 @@ return {
       -- lint.linters_by_ft['ruby'] = nil
       -- lint.linters_by_ft['terraform'] = nil
       -- lint.linters_by_ft['text'] = nil
-
-      -- Create autocommand which carries out the actual linting
-      -- on the specified events.
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
-          -- Only run the linter in buffers that you can modify in order to
-          -- avoid superfluous noise, notably within the handy LSP pop-ups that
-          -- describe the hovered symbol using Markdown.
-          if vim.opt_local.modifiable:get() then
-            lint.try_lint()
-          end
-        end,
-      })
     end,
   },
 }
