1640058725f4fa7135da4bda6a98702b915be300
mactavishz <maczhanchao@yahoo.com>
2026-01-08 15:06:25 +0100

    Fix broken treesitter major update

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,61 +1,136 @@
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     build = ':TSUpdate',
-    branch = 'master',
     lazy = false,
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = {
-          'go',
-          'rust',
-          'ruby',
-          'bash',
-          'c',
-          'diff',
-          'html',
-          'lua',
-          'luadoc',
-          'markdown',
-          'markdown_inline',
-          'query',
-          'vim',
-          'vimdoc',
-          'astro',
-          'css',
-        },
-        -- Autoinstall languages that are not installed
-        auto_install = true,
-        highlight = {
-          enable = true,
-          -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-          --  If you are experiencing weird indenting issues, add the language to
-          --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-          additional_vim_regex_highlighting = { 'ruby' },
-        },
-        indent = { enable = true, disable = { 'ruby' } },
-      }
+    opts = {
+      ensure_installed = {
+        'go',
+        'gomod',
+        'gowork',
+        'gosum',
+        'rust',
+        'ruby',
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'astro',
+        'css',
+        'latex',
+      },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+      folds = {
+        enable = true,
+      },
+      indent = { enable = true },
+    },
+    config = function(_, opts)
+      local TS = require 'nvim-treesitter'
+      -- setup treesitter
+      TS.setup(opts)
+      TS.install(opts.ensure_installed, { summary = true })
+
+      -- Enable treesitter features via FileType autocmd
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('treesitter_features', { clear = true }),
+        callback = function(ev)
+          local buf = ev.buf
+          local ft = ev.match
+          local lang = vim.treesitter.language.get_lang(ft) or ft
+
+          -- Check if parser is available for this language
+          local ok = pcall(vim.treesitter.language.add, lang)
+          if not ok then
+            return
+          end
+
+          -- Enable highlighting
+          if opts.highlight and opts.highlight.enable then
+            pcall(vim.treesitter.start, buf, lang)
+          end
+
+          -- Enable folds (set for current window)
+          if opts.folds and opts.folds.enable then
+            vim.wo.foldmethod = 'expr'
+            vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          end
+
+          -- Enable indentation
+          if opts.indent and opts.indent.enable then
+            vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
     end,
   },
   {
     'nvim-treesitter/nvim-treesitter-context',
-    dependencies = {
-          'nvim-treesitter/nvim-treesitter',
-    }
+    event = 'VeryLazy',
+    opts = function()
+      local tsc = require 'treesitter-context'
+      Snacks.toggle({
+        name = 'Treesitter Context',
+        get = tsc.enabled,
+        set = function(state)
+          if state then
+            tsc.enable()
+          else
+            tsc.disable()
+          end
+        end,
+      }):map '<leader>ut'
+      return { mode = 'cursor', max_lines = 3 }
+    end,
   },
   {
     'nvim-treesitter/nvim-treesitter-textobjects',
-    dependencies = {
-          'nvim-treesitter/nvim-treesitter',
-    }
+    branch = 'main',
+    event = 'VeryLazy',
+    init = function()
+      -- Disable entire built-in ftplugin mappings to avoid conflicts.
+      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+      vim.g.no_plugin_maps = true
+
+      -- Or, disable per filetype (add as you like)
+      -- vim.g.no_python_maps = true
+      -- vim.g.no_ruby_maps = true
+      -- vim.g.no_rust_maps = true
+      -- vim.g.no_go_maps = true
+    end,
+    opts = {
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        -- LazyVim extention to create buffer-local keymaps
+        keys = {
+          goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
+          goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
+          goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
+          goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' },
+        },
+      },
+    },
+    config = function(_, opts)
+      local TS = require 'nvim-treesitter-textobjects'
+      TS.setup(opts)
+    end,
   },
   {
     'windwp/nvim-ts-autotag',
-    event = 'VeryLazy',
     opts = {},
-    dependencies = {
-          'nvim-treesitter/nvim-treesitter',
-    }
   },
 }
