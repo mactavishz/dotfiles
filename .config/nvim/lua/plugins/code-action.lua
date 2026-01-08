0066c4d028f7c8793a7da696a184da1e7f16171c
mactavishz <maczhanchao@yahoo.com>
2026-01-08 14:02:20 +0100

    Update code-action config

--- .config/nvim/lua/plugins/code-action.lua
+++ .config/nvim/lua/plugins/code-action.lua
@@ -3,43 +3,17 @@ return {
   dependencies = {
     { 'nvim-lua/plenary.nvim' },
     {
-      "folke/snacks.nvim",
+      'folke/snacks.nvim',
       opts = {
         terminal = {},
-      }
-    }
+      },
+    },
   },
   event = 'LspAttach',
-  opts = {},
-  config = function()
-    vim.keymap.set({ 'n', 'x' }, '<leader>ca', function()
-      require('tiny-code-action').code_action()
-    end, { noremap = true, silent = true })
-    require('tiny-code-action').setup {
-      picker = {
-        'buffer',
-        opts = {
-          hotkeys = true, -- Enable hotkeys for quick selection of actions
-          -- Use numeric labels.
-          hotkeys_mode = function(titles)
-            return vim
-              .iter(ipairs(titles))
-              :map(function(i)
-                return tostring(i)
-              end)
-              :totable()
-          end,
-          auto_preview = true, -- Enable or disable automatic preview
-          auto_accept = false, -- Automatically accept the selected action
-          position = 'cursor', -- Position of the picker window
-          winborder = 'single', -- Border style for picker and preview windows
-          custom_keys = {
-            { key = 'f', pattern = 'Fill match arms' },
-            { key = 'f', pattern = 'Fill struct fields' },
-          },
-          notify = { enabled = false },
-        },
-      },
-    }
-  end,
+  opts = {
+    picker = 'snacks',
+  },
+  keys = {
+    { '<leader>ca', function() require('tiny-code-action').code_action() end, desc = 'Open Code Action' },
+  },
 }
