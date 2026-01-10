09cf743635fc16c413cf3c09ffd94ba96999ed96
mactavishz <maczhanchao@yahoo.com>
2026-01-10 15:54:23 +0100

    Update some keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -38,13 +38,23 @@ vim.keymap.set("v", ">", ">gv")
 vim.keymap.set({ "n", "o", "x" }, "<s-h>", "^", { desc = "Jump to beginning of line" })
 vim.keymap.set({ "n", "o", "x" }, "<s-l>", "g_", { desc = "Jump to end of line" })
 
+-- Keep window centered when going up/down
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Select all
+vim.keymap.set("n", "==", "gg<S-v>G")
+
 if not is_vscode then
   -- Save and quit
   -- vim.keymap.set('n', 'QQ', ':q!<enter>', { noremap = false })
   -- vim.keymap.set('n', 'WW', ':w!<enter>', { noremap = false })
   -- Move block
   vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Block Down" })
-  vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Block Up" })
+  vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move Block Up" })
   vim.keymap.set("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Execute the current line" })
   vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute the current file" })
 
