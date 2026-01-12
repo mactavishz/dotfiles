c094e496b6c3f74be373e6b1e4a4ba8c14a3a73e
mactavishz <maczhanchao@yahoo.com>
2026-01-12 12:30:41 +0100

    Add ghostty config, update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -42,35 +42,6 @@ config.keys = {
 		mods = "CMD|SHIFT",
 		action = act.ShowTabNavigator,
 	},
-	{
-		key = "R",
-		mods = "CMD|SHIFT",
-		action = act.PromptInputLine({
-			description = "Enter new name for tab",
-			action = wezterm.action_callback(function(window, _, line)
-				-- line will be `nil` if they hit escape without entering anything
-				-- An empty string if they just hit enter
-				-- Or the actual line of text they wrote
-				if line then
-					window:active_tab():set_title(line)
-				end
-			end),
-		}),
-	},
-	{
-		key = ",",
-		mods = "CMD",
-		action = act.SpawnCommandInNewTab({
-			cwd = os.getenv("WEZTERM_CONFIG_DIR"),
-			set_environment_variables = {
-				TERM = "screen-256color",
-			},
-			args = {
-				"/usr/local/bin/nvim",
-				os.getenv("WEZTERM_CONFIG_FILE"),
-			},
-		}),
-	},
 	-- other keys
 }
 
