7b50b77e86f75d8ca4aa8b1080895229620c9c44
mactavishz <maczhanchao@yahoo.com>
2026-01-12 09:56:18 +0100

    Add mason-nvim-lint to automatically install linters

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,24 +1,38 @@
 -- Thanks to: https://www.youtube.com/watch?v=yI9R13h9IEE
 return {
-  "WhoIsSethDaniel/mason-tool-installer.nvim",
-  dependencies = {
-    {
-      "neovim/nvim-lspconfig",
-      dependencies = {
-        { "j-hui/fidget.nvim", opts = {} },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = {
+      {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+          { "j-hui/fidget.nvim", opts = {} },
+        },
       },
+      -- With this we don't have to call require("mason").setup()
+      { "mason-org/mason.nvim", opts = {} },
+      -- With this we don't have to call require("mason-lspconfig").setup()
+      { "mason-org/mason-lspconfig.nvim", opts = {} },
     },
-    -- With this we don't have to call require("mason").setup()
-    { "mason-org/mason.nvim", opts = {} },
-    -- With this we don't have to call require("mason-lspconfig").setup()
-    { "mason-org/mason-lspconfig.nvim", opts = {} },
-  },
-  config = function()
-    -- Automatically detect LSP configs from lsp folder
-    local ensure_installed = require("core.lsp").lsp_list
+    config = function()
+      -- Automatically detect LSP configs from lsp folder
+      local ensure_installed = require("core.lsp").lsp_list
 
-    require("mason-tool-installer").setup({
-      ensure_installed = vim.tbl_deep_extend("force", ensure_installed, {}),
-    })
-  end,
+      require("mason-tool-installer").setup({
+        ensure_installed = vim.tbl_deep_extend("force", ensure_installed, {}),
+      })
+    end,
+  },
+  {
+    "rshkarin/mason-nvim-lint",
+    cond = vim.g.vscode == nil,
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-lint",
+    },
+    opts = {
+      ensure_installed = {},
+      automatic_installation = true,
+    },
+  },
 }
